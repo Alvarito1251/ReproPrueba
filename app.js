ecbe2758b21588af868facef0366c3821bb0fae7
JuanLavallen <juanlavallen02@gmail.com>
2021-10-21 10:59:39 -0300

    Codigo JS

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// Modo Oscuro
+const btnSwitch = document.querySelector("#switch");
+const nav = document.querySelector("#nav")
+
+btnSwitch.addEventListener("click", function(){
+    document.body.classList.toggle("dark");
+    btnSwitch.classList.toggle("active");
+    nav.classList.toggle("bg-light");
+    nav.classList.toggle("bg-dark");
+    nav.classList.toggle("navbar-light");
+    nav.classList.toggle("navbar-dark");
+})
+
 // Login
 function iniciarSesion(){
     let usuario = document.getElementById('user').value
